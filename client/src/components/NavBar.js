ea12e0058d0693b27c2a59b9fc699378b7614a9e
sharon <sharonchepkirui665@gmail.com>
2023-03-28 14:03:48 +0300

    commit

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,16 @@
+
+import {NavLink} from "react-router-dom";
+
+function NavBar(){
+    return(
+        <div className="flex justify-center gap-10">
+           <NavLink to="/">Home</NavLink>
+           <NavLink to="/about">About</NavLink>
+           <NavLink to="/logIn">LogIn</NavLink>
+           <NavLink to="/signUp">SignUp</NavLink>
+           <NavLink to="/logout">LogOut</NavLink>
+           
+        </div>
+    )
+}
+export default NavBar;
