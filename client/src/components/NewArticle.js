3521541bce20cd37d05bffde587f43cb9fca7738
Bloom <bloomtauta@gmail.com>
2023-04-03 11:12:57 +0300

    login

--- client/src/components/NewArticle.js
+++ client/src/components/NewArticle.js
@@ -143,9 +143,321 @@
 // export default NewArticle;
 
 
-import React, { useState, useEffect } from 'react';
-import './newarticle.css';
+// import React, { useState, useEffect } from 'react';
+// import './newarticle.css';
 
+// function NewArticle() {
+//   const [categories, setCategories] = useState([]);
+//   const [article, setArticle] = useState({
+//     name: '',
+//     genre: '',
+//     rating: '',
+//     img_url: '',
+//     user_id: '',
+//     category_id: '',
+//   });
+
+//   useEffect(() => {
+//     fetch('/categories')
+//       .then(response => response.json())
+//       .then(data => {
+//         setCategories(data);
+//       })
+//       .catch(error => console.error('Error fetching categories:', error));
+//   }, []);
+
+//   function addNewArticle(newArticle) {
+//     setArticle({
+//       name: '',
+//       genre: '',
+//       rating: '',
+//       img_url: '',
+//       user_id: '',
+//       category_id: '',
+//     });
+//   }
+
+//   const handleChange = event => {
+//     const { name, value } = event.target;
+//     setArticle(prevArticle => ({
+//       ...prevArticle,
+//       [name]: value
+//     }));
+//   };
+
+//   const handleSubmit = event => {
+//     event.preventDefault();
+
+//     fetch('/articles', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(article)
+//     })
+//       .then(response => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw new Error('Network response was not ok');
+//         }
+//       })
+//       .then(data => {
+//         addNewArticle(data);
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//       });
+//   };
+//   return (
+//     <div>
+//     <h1>New Article</h1>
+//     <form onSubmit={handleSubmit}>
+//     <div>
+//     <label htmlFor="name">Title:</label>
+//     <input
+//              type="text"
+//              id="name"
+//              name="name"
+//              value={article.name}
+//              onChange={handleChange}
+//            />
+//     </div>
+//     <div>
+//     <label htmlFor="genre">Editorial:</label>
+//     <input
+//              type="text"
+//              id="genre"
+//              name="genre"
+//              value={article.genre}
+//              onChange={handleChange}
+//            />
+//     </div>
+//     <div>
+//     <label htmlFor="rating">Rating:</label>
+//     <input
+//              type="number"
+//              id="rating"
+//              name="rating"
+//              value={article.rating}
+//              onChange={handleChange}
+//            />
+//     </div>
+//     <div>
+//     <label htmlFor="img_url">Image URL:</label>
+//     <input
+//              type="text"
+//              id="img_url"
+//              name="img_url"
+//              value={article.img_url}
+//              onChange={handleChange}
+//            />
+//     </div>
+//     <div>
+//     <label htmlFor="user_id">User_id:</label>
+//     <input
+//              type="text"
+//              id="user_id"
+//              name="user_id"
+//              value={article.user_id}
+//              onChange={handleChange}
+//            />
+//     </div>
+//     <div>
+//     <label htmlFor="category_id">Category</label>
+//     <select
+//              id="category_id"
+//              name="category_id"
+//              value={article.category}
+//              onChange={handleChange}
+//            >
+//     <option value="">--Select a category--</option>
+//     {categories.map(category => (
+//     <option key={category.id} value={category.id}>
+//     {category.content}
+//     </option>
+//     ))}
+//     </select>
+//     </div>
+//     <button type="submit">Create Article</button>
+//     </form>
+//     </div>
+//     );
+//     }
+    
+//     export default NewArticle;
+
+// import React, { useState, useEffect } from 'react';
+// import './newarticle.css'
+// import { useNavigate } from 'react-router-dom';
+// function NewArticle() {
+//   const [categories, setCategories] = useState([]);
+//   const [article, setArticle] = useState({
+//     name: '',
+//     genre: '',
+//     rating: '',
+//     img_url: '',
+//     user_id: '',
+//     category_id: '',
+//   });
+//   const [currentUser, setCurrentUser] = useState(null);
+//   useEffect(() => {
+//     const token = localStorage.token;
+//     if (typeof token !== 'undefined' && token.length > 1 && token !== 'undefined') {
+//       fetch('/auto_login', {
+//         method: 'POST',
+//         headers: {
+//           Accept: 'application/json',
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ token }),
+//       })
+//         .then((r) => r.json())
+//         .then((user) => setCurrentUser(user));
+//     } else {
+//       console.log('No token found, try logging in!');
+//     }
+//     fetch('/categories')
+//       .then(response => response.json())
+//       .then(data => {
+//         setCategories(data);
+//       })
+//       .catch(error => console.error('Error fetching categories:', error));
+//   }, []);
+//   function addNewArticle(newArticle) {
+//     setArticle({
+//       name: '',
+//       genre: '',
+//       rating: '',
+//       img_url: '',
+//       user_id: '',
+//       category_id: '',
+//     });
+//   }
+//   const handleChange = event => {
+//     const { name, value } = event.target;
+//     setArticle(prevArticle => ({
+//       ...prevArticle,
+//       [name]: value
+//     }));
+//   };
+//   const navigate = useNavigate();
+//   const handleSubmit = event => {
+//     event.preventDefault();
+//     setArticle(prevArticle => ({
+//       ...prevArticle,
+//       user_id: currentUser.id,
+//     }));
+//     fetch('/articles', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(article)
+//     })
+//       .then(response => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw new Error('Network response was not ok');
+//         }
+//       })
+//       .then(data => {
+//         addNewArticle(data);
+//         navigate(`/articles`); // Redirect to the newly created article page
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//       });
+//   };
+//   return (
+// <div className="article-form-container">
+//   <h1>New Article</h1>
+//   <form onSubmit={handleSubmit} className="article-form">
+//     <div className="form-group">
+//       <label htmlFor="name">Name:</label>
+//       <input
+//         type="text"
+//         id="name"
+//         name="name"
+//         value={article.name}
+//         onChange={handleChange}
+//         className="form-control"
+//       />
+//     </div>
+//     <div className="form-group">
+//       <label htmlFor="genre">Genre:</label>
+//       <input
+//         type="text"
+//         id="genre"
+//         name="genre"
+//         value={article.genre}
+//         onChange={handleChange}
+//         className="form-control"
+//       />
+//     </div>
+//     <div className="form-group">
+//       <label htmlFor="rating">Rating:</label>
+//       <input
+//         type="number"
+//         id="rating"
+//         name="rating"
+//         value={article.rating}
+//         onChange={handleChange}
+//         className="form-control"
+//       />
+//     </div>
+//     <div className="form-group">
+//       <label htmlFor="img_url">Image URL:</label>
+//       <input
+//         type="text"
+//         id="img_url"
+//         name="img_url"
+//         value={article.img_url}
+//         onChange={handleChange}
+//         className="form-control"
+//       />
+//     </div>
+//     <div className="form-group">
+//       <label htmlFor="user_id">Username:</label>
+//       <input
+//         type="text"
+//         id="user_id"
+//         name="user_id"
+//         value={currentUser?.username || ""}
+//         onChange={handleChange}
+//         disabled
+//         className="form-control"
+//       />
+//     </div>
+//     <div className="form-group">
+//       <label htmlFor="category_id">Category</label>
+//       <select
+//         id="category_id"
+//         name="category_id"
+//         value={article.category_id}
+//         onChange={handleChange}
+//         className="form-control"
+//       >
+//         <option value="">--Select a category--</option>
+//         {categories.map(category => (
+//           <option key={category.id} value={category.id}>
+//             {category.content}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//     <button type="submit" className="btn btn-primary">Create Article</button>
+//   </form>
+// </div>
+//     );
+//     }
+//     export default NewArticle;
+    
+    
+import React, { useState, useEffect } from 'react';
+import './newarticle.css'
+import { useNavigate } from 'react-router-dom';
 function NewArticle() {
   const [categories, setCategories] = useState([]);
   const [article, setArticle] = useState({
@@ -156,8 +468,23 @@ function NewArticle() {
     user_id: '',
     category_id: '',
   });
-
+  const [currentUser, setCurrentUser] = useState(null);
   useEffect(() => {
+    const token = localStorage.token;
+    if (typeof token !== 'undefined' && token.length > 1 && token !== 'undefined') {
+      fetch('/auto_login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+      })
+        .then((r) => r.json())
+        .then((user) => setCurrentUser(user));
+    } else {
+      console.log('No token found, try logging in!');
+    }
     fetch('/categories')
       .then(response => response.json())
       .then(data => {
@@ -165,7 +492,6 @@ function NewArticle() {
       })
       .catch(error => console.error('Error fetching categories:', error));
   }, []);
-
   function addNewArticle(newArticle) {
     setArticle({
       name: '',
@@ -176,7 +502,6 @@ function NewArticle() {
       category_id: '',
     });
   }
-
   const handleChange = event => {
     const { name, value } = event.target;
     setArticle(prevArticle => ({
@@ -184,10 +509,13 @@ function NewArticle() {
       [name]: value
     }));
   };
-
+  const navigate = useNavigate();
   const handleSubmit = event => {
     event.preventDefault();
-
+    setArticle(prevArticle => ({
+      ...prevArticle,
+      user_id: currentUser.id,
+    }));
     fetch('/articles', {
       method: 'POST',
       headers: {
@@ -204,88 +532,93 @@ function NewArticle() {
       })
       .then(data => {
         addNewArticle(data);
+        navigate(`/articles`); // Redirect to the newly created article page
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
       });
   };
   return (
-    <div>
-    <h1>New Article</h1>
-    <form onSubmit={handleSubmit}>
-    <div>
-    <label htmlFor="name">Title:</label>
-    <input
-             type="text"
-             id="name"
-             name="name"
-             value={article.name}
-             onChange={handleChange}
-           />
+<div className="article-form-container">
+  <h1>New Article</h1>
+  <form onSubmit={handleSubmit} className="article-form">
+    <div className="form-group">
+      <label htmlFor="name">Title:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={article.name}
+        onChange={handleChange}
+        className="form-control"
+      />
     </div>
-    <div>
-    <label htmlFor="genre">Editorial:</label>
-    <input
-             type="text"
-             id="genre"
-             name="genre"
-             value={article.genre}
-             onChange={handleChange}
-           />
+    <div className="form-group">
+      <label htmlFor="genre">Editorial:</label>
+      <input
+        type="text"
+        id="genre"
+        name="genre"
+        value={article.genre}
+        onChange={handleChange}
+        className="form-control"
+      />
     </div>
-    <div>
-    <label htmlFor="rating">Rating:</label>
-    <input
-             type="number"
-             id="rating"
-             name="rating"
-             value={article.rating}
-             onChange={handleChange}
-           />
+    <div className="form-group">
+      <label htmlFor="rating">Rating:</label>
+      <input
+        type="number"
+        id="rating"
+        name="rating"
+        value={article.rating}
+        onChange={handleChange}
+        className="form-control"
+      />
     </div>
-    <div>
-    <label htmlFor="img_url">Image URL:</label>
-    <input
-             type="text"
-             id="img_url"
-             name="img_url"
-             value={article.img_url}
-             onChange={handleChange}
-           />
+    <div className="form-group">
+      <label htmlFor="img_url">Image URL:</label>
+      <input
+        type="text"
+        id="img_url"
+        name="img_url"
+        value={article.img_url}
+        onChange={handleChange}
+        className="form-control"
+      />
     </div>
-    <div>
-    <label htmlFor="user_id">User_id:</label>
-    <input
-             type="text"
-             id="user_id"
-             name="user_id"
-             value={article.user_id}
-             onChange={handleChange}
-           />
+    <div className="form-group">
+      <label htmlFor="user_id">Username:</label>
+      <input
+        type="text"
+        id="user_id"
+        name="user_id"
+        value={currentUser?.username || ""}
+        onChange={handleChange}
+        disabled
+        className="form-control"
+      />
     </div>
-    <div>
-    <label htmlFor="category_id">Category</label>
-    <select
-             id="category_id"
-             name="category_id"
-             value={article.category}
-             onChange={handleChange}
-           >
-    <option value="">--Select a category--</option>
-    {categories.map(category => (
-    <option key={category.id} value={category.id}>
-    {category.content}
-    </option>
-    ))}
-    </select>
+    <div className="form-group">
+      <label htmlFor="category_id">Category</label>
+      <select
+        id="category_id"
+        name="category_id"
+        value={article.category_id}
+        onChange={handleChange}
+        className="form-control"
+      >
+        <option value="">--Select a category--</option>
+        {categories.map(category => (
+          <option key={category.id} value={category.id}>
+            {category.content}
+          </option>
+        ))}
+      </select>
     </div>
-    <button type="submit">Create Article</button>
-    </form>
-    </div>
+    <button type="submit" className="btn btn-primary">Create Article</button>
+  </form>
+</div>
     );
     }
-    
     export default NewArticle;
-
-
 
