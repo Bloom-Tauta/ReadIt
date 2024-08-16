33a84ec3a16b1d982e150070f6dda4e37142f56a
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-16 12:59:23 +0300

    final changes

--- app/serializers/article_list.rb
+++ app/serializers/article_list.rb
@@ -1,3 +1,3 @@
 class ArticleListSerializer < ActiveModel::Serializer
-    attributes :id, :img_url, :genre, :name, :rating, :author, :preview, 
+    attributes :id, :img_url, :genre, :name, :rating, :author, :preview, :date
 end
